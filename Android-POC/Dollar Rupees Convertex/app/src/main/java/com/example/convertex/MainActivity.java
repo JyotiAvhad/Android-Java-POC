d516f64228891bbc6de69081e7cc5152bd99835f
Jyoti Avhad <avhadjyoti09@gmail.com>
2019-10-22 12:45:46 +0530

    Implement Dollar Rupees Conveter

---
+++ Android-POC/Dollar Rupees Convertex/app/src/main/java/com/example/convertex/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.convertex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_dollars;
+    TextView tv_rupees;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_dollars = findViewById(R.id.editText_dollar);
+        tv_rupees = findViewById(R.id.textView_rupees);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String dollars = et_dollars.getText().toString();
+                Double dDollars = Double.parseDouble(dollars);
+
+                Double dollarsINR = 70.67 * dDollars;//for dollars to rupees conversion
+//                Double dollarsINR = 70.67 / dDollars;//for rupees to dollars conversion
+
+                tv_rupees.setText(String.valueOf(dollarsINR)+" ₹");
+            }
+        });
+    }
+
+}
